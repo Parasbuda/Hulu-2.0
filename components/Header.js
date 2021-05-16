1c1343b8f792c9774255703927e422e76efe61b8
Parasbuda <60026182+Parasbuda@users.noreply.github.com>
2021-05-16 10:34:10 +0545

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Image from "next/image"
+const Header = () => {
+  return (
+    <header>
+      <Image className="object-contain" src="https://links.papareact.com/ua6" width={200} height={100}/>
+    </header>
+  );
+};
+
+export default Header;
